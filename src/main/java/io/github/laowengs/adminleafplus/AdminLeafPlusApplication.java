0c45c818ae5e4f12ba83bed3877c7d09fe44a7a1
laowengs <229595412@qq.com>
2024-07-21 12:55:55 +0800

    init

---
+++ src/main/java/io/github/laowengs/adminleafplus/AdminLeafPlusApplication.java
@@ -0,0 +1,13 @@
+package io.github.laowengs.adminleafplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminLeafPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminLeafPlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/laowengs/adminleafplus/AdminLeafPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.laowengs.adminleafplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminLeafPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
